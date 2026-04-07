28227e7a2fa07252bb08ac670276bd0127f103d7
Drake2209 <drake.carcellar16@gmail.com>
2026-04-07 11:09:45 +0800

    -add eslint file for eslint error detection

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+  rules: {
+    'no-unused-vars': 'warn', // was 'error'
+    'vue/no-unused-vars': 'warn', // was 'error'
+    'no-undef': 'error', // keep this as error
+    'no-empty': 'warn', // was 'error'
+  },
+}
