23498cea688b3f762ca023f8e6575f06a1136dd0
Drake2209 <drake.carcellar16@gmail.com>
2026-05-30 13:34:13 +0800

    feat: add isAdmin getter to auth store

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -9,6 +9,7 @@ export const useAuthStore = defineStore('auth', {
 
   getters: {
     isAuthenticated: (state) => !!state.token,
+    isAdmin: (state) => state.user?.role === 'admin',
   },
 
   actions: {
